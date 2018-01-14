e4c9113464163657f1910ec6c87939f7e62fa6c3
Groka <tarik.upss@gmail.com>
2018-01-14 02:26:42 +0100

    bugfix

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@softskillsacademy.ba'),
+        'name' => env('MAIL_FROM_NAME', 'SSA \'' . date('y')),
     ],
 
     /*
