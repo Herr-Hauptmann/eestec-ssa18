0420668f457350426d3b7963ebc123551aba6081
_Hauptmann_ <tarik.horoz@gmail.com>
2020-01-06 22:09:37 +0100

    Typo edit
    - edited out a typo
    - added more details for example.blade

---
+++ resources/views/partials-2020/example.blade.php
@@ -0,0 +1,41 @@
+<!-- OVDJE MOZETE VIDJETI PRIMJER KAKO JE DO SADA IZGLEDAO BLADE O PROJEKTU, DAKLE, DIREKTNO KUCATE HTML KOD -->
+
+<section class="container-fluid marginContainer oprojektu-cont">
+    <p class="sectionHeadline"><b>o projektu</b></p>
+    <p class="sectionHeadlineBottomRed"></p>
+    <div class="row aboutProjectRowMargin">
+        <div class="col-md-5 aboutProjectImgDiv">
+            <div class="marginRight30">
+                <img class="img-responsive aboutProjectImg" src="{{ asset('img/proba1.jpg') }}" />
+            </div>
+        </div>
+        <div class="col-md-7 justifyText">
+            <p class="aboutProjectTextHeader">Šta je SSA?</p>
+            <p class="aboutProjectBottomRed"></p>
+            <p class="aboutProjectTextTop">
+                Soft Skills Academy Sarajevo je besplatna trodnevna radionica ličnih i profesionalnih vještina za studente svih fakulteta u Sarajevu, koja podstiče studente na razvoj neformalnih vještina neophodnih za zapošljavanje i uspješnu karijeru, ali i za izazove modernog života, koje tradicionalan način studiranja ne može obezbijediti. Ovogodišnji SSA se održava u periodu od 6. do 8. marta 2020. godine.
+
+            </p>
+            <p class="aboutProjectTextBottom">
+                <!-- dodati teme kad budemo imali teme -->
+            </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-7 justifyText">
+            <p class="aboutProjectTextHeader">Treninzi</p>
+            <p class="aboutProjectBottomRed"></p>
+            <p class="aboutProjectTextTop">
+                Treninge će držati certificirani soft skills treneri. Po završetku radionice učesnici dobijaju certifikat i CV dodatak potpisan od strane Organizacionog odbora i odgovornih trenera, koji će moći priložiti uz CV prilikom zapošljavanja.
+            </p>
+            <p class="aboutProjectTextBottom">
+                Usavršavanjem ovih vještina, neophodnih za uspješno i efikasno bavljenje različitim poslovima, učesnici koji budu prisustvovali radionici će steći prednost nad konkurencijom i zasigurno obezbijediti sebi kvalitetan posao. Osim toga, steći će vještine koje ne posjeduju, ali i razviti one koje su im nedovoljno razvijene, što će pozitivno utjecati na njihovu samosvjesnost i samopouzdanje. 
+            </p>
+        </div>
+        <div class="col-md-5 aboutProjectImgDiv">
+            <div class="marginLeft30">
+                <img class="img-responsive aboutProjectImg" src="{{ asset('img/proba2.jpg') }}" />
+            </div>
+        </div>
+    </div>
+</section>
