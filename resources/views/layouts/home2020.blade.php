96b8b6d29439d21a6d7033ad1fad4dcf48f85bdf
_Hauptmann_ <tarik.horoz@gmail.com>
2020-02-11 23:16:17 +0100

    NE ZNAM ALLAHA MI

--- resources/views/layouts/home2020.blade.php
+++ resources/views/layouts/home2020.blade.php
@@ -84,5 +84,13 @@
             $('.match-height').matchHeight();
         });
     </script>
+
+    <script type="text/javascript">
+    //auto expand textarea
+    function adjust_textarea(h) {
+        h.style.height = "20px";
+        h.style.height = (h.scrollHeight)+"px";
+    }
+    </script>
 </body>
 </html>
