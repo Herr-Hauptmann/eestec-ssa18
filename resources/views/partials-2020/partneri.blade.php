38ea3a5eddb9026c6e30a6bea685511907e60220
@setic.merjema <setic.merjema@gmail.com>
2020-02-26 23:21:02 +0100

    Počela partnere

---
+++ resources/views/partials-2020/partneri.blade.php
@@ -0,0 +1,8 @@
+<div class="container-fluid svi_partneri mt-5">
+    <div class="row justify-content-center prvi_red pt-5">
+        Partneri
+    </div>
+
+    <div class="row justify-content-center drugi_red pt-2">
+        Soft Skills Academy su do sada podržali:
+    </div>
