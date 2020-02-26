502e9612a039bb0f15d90b424c9773682fb1d0ab
@setic.merjema <setic.merjema@gmail.com>
2020-02-27 00:23:38 +0100

    Pokusaj ubacivanja partnera

--- resources/views/partials-2020/partneri.blade.php
+++ resources/views/partials-2020/partneri.blade.php
@@ -6,3 +6,21 @@
     <div class="row justify-content-center drugi_red pt-2">
         Soft Skills Academy su do sada podržali:
     </div>
+
+    <div class="row justify-content-center img-fluid glavni_dio pt-4 pb-5">
+        <div class="col-2 kolona1">
+            <div class="row justify-content-center kolona1_red1"><img src="https://images.footlocker.com/content/dam/final/champs/site/homepage/20191106-Brands-Nike-6up.jpg" ></div> 
+            <div class="row justify-content-center kolona1_red2"><img src="https://images.footlocker.com/content/dam/final/champs/site/homepage/20191106-Brands-Nike-6up.jpg" ></div> 
+        </div>
+
+         <div class="col-2 kolona2">
+            <div class="row justify-content-center kolona2_red1"><img src="https://images.footlocker.com/content/dam/final/champs/site/homepage/20191106-Brands-Nike-6up.jpg" ></div> 
+            <div class="row justify-content-center kolona2_red2"><img src="https://images.footlocker.com/content/dam/final/champs/site/homepage/20191106-Brands-Nike-6up.jpg" ></div> 
+        </div>
+
+
+    </div>
+
+
+  
+</div>
