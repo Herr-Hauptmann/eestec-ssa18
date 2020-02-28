5900f214c0465095f33962fa78bb5a568e3369bd
unknown <arijana.colak1@gmail.com>
2020-02-28 17:41:39 +0100

    radjen footer

--- resources/views/home/index2020.blade.php
+++ resources/views/home/index2020.blade.php
@@ -16,6 +16,7 @@
     <link rel="stylesheet" href="{{ asset('css/ssa-2020/organizatori.css')}}">
     <link rel="stylesheet" href="{{ asset('css/ssa-2020/partneri.css')}}">
     <link rel="stylesheet" href="{{ asset('css/ssa-2020/postignuca.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/ssa-2020/footer.css')}}">
     <script src="{{ asset('js/ssa20/kontakt.js') }}"></script>
     <script src="https://kit.fontawesome.com/4c9ea8fa84.js" crossorigin="anonymous"></script>
 @endsection
