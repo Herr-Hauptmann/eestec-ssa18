e40a52b98a2f92b371321cdb947aeba0369a8d35
Seid Korac <koracseid5@gmail.com>
2020-03-29 01:18:27 +0100

    dodane sve novosti

--- resources/views/partials-2020/novosti.blade.php
+++ resources/views/partials-2020/novosti.blade.php
@@ -1,15 +1,25 @@
 <div class="container novosti-kontejner" id="novosti">
     <div class="row">
+      <div class="col-6 text-left">
         <h1 class="news-heading">
-            Novosti
+          Novosti
         </h1>
-    </div>
+      </div>
 
-    <div class="row justify-content-around">
+      <div class="col-6 text-right">
+        <i onclick="scrollL()" class="fas fa-long-arrow-alt-left strijele"></i>
+        <i onclick="scrollR()" class="fas fa-long-arrow-alt-right strijele"></i>
+      </div>
+
+    </div>
+    @php
+        $newsCount= count($posts);
+    @endphp
+    <div class="row flex-nowrap  overflow-hidden ">
         
         @foreach($posts as $post)
 
-            <div class='col-6 col-lg-3 p-2'>
+            <div class='col-12 col-sm-6 col-md-4 col-lg-3 p-2 novostba'>
                 <a href="{{ route('jednaNovost', $post->id) }}">
                     <div class="novost-div">
 
@@ -42,3 +52,40 @@
     </div>
 
 </div>
+
+<script>
+    var pixls = screen.width;
+    if (pixls < 576) {
+      var mks = {{ $newsCount}} -1 ;
+    } else if(pixls < 768){
+      var mks = {{$newsCount}} -2;
+    } else if(pixls < 992){
+      var mks = {{$newsCount}} -3;
+    } else{
+      var mks = {{$newsCount}} -4;
+    }
+    var pg = 0;
+    // treba ubaciti provjeru za zadnju stranu
+  
+  
+  
+    function scrollL() {
+      if (pg > 0) {
+        demoId = document.querySelectorAll('.novostba');
+        --pg;
+        demoId.forEach(element => {
+          element.style.transform = 'translateX(' + -(pg) + '00%)';
+        });
+      }
+    }
+  
+    function scrollR() {
+      if (pg < mks) {
+        demoId2 = document.querySelectorAll('.novostba');
+        ++pg;
+        demoId2.forEach(element => {
+          element.style.transform = 'translateX(' + -(pg) + '00%)';
+        });
+      }
+    }
+  </script>
