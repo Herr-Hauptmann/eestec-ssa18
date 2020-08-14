e7f405cd88ca3af54b636bd2bb350b05d5eed083
Seid-Korac <skorac1@etf.unsa.ba>
2020-08-14 18:46:50 +0200

    minor partner fixes

--- resources/views/lite/lite-partials/partneri.blade.php
+++ resources/views/lite/lite-partials/partneri.blade.php
@@ -5,37 +5,37 @@
     <div class="row logoContainer center-hv">
         <div class="logoDiv-medij col-md-3 col-4">
             <a href="https://www.marketmakers.ba" target="_blank">
-                <div alt="marketMakers" class="div-responsive_img" style="background-image: url({{ asset(img/lite/partneri/MarketMakers.png) }})"></div>
+                <div alt="marketMakers" class="div-responsive_img" style="background-image: url({{ asset('/img/lite/partneri/MarketMakers.png') }})"></div>
             </a>
         </div>
         <div class="logoDiv-medij col-md-3 col-4">
             <a href="https://gocloud.group" target="_blank">
-                <div alt="goCloud" class="div-responsive_img" style="background-image: url({{ asset(img/lite/partneri/GoCloud.png) }})"></div>
+                <div alt="goCloud" class="div-responsive_img" style="background-image: url({{ asset('/img/lite/partneri/GoCloud.png') }})"></div>
             </a>
         </div>
         <div class="logoDiv-medij col-md-3 col-4">
-            <a href="#" target="_blank">
-                <div alt="historijskiMuzej" class="div-responsive_img" style="background-image: url({{ asset(img/lite/partneri/historijskiMuzejBiH.png) }})"></div>
+            <a href="https://muzej.ba/" target="_blank">
+                <div alt="historijskiMuzej" class="div-responsive_img" style="background-image: url({{ asset('/img/lite/partneri/historijskiMuzejBiH.png') }})"></div>
             </a>
         </div>
         <div class="logoDiv-medij col-md-3 col-4">
             <a href="https://www.dzajic-commerce.com" target="_blank">
-                <div alt="dzajic" class="div-responsive_img" style="background-image: url({{ asset(img/lite/partneri/dzajic.png) }})"></div>
+                <div alt="dzajic" class="div-responsive_img" style="background-image: url({{ asset('/img/lite/partneri/dzajic.png') }})"></div>
             </a>
         </div>
         <div class="logoDiv-medij col-md-3 col-4">
             <a href="https://www.omcafe.net" target="_blank">
-                <div alt="omcafe" class="div-responsive_img" style="background-image: url({{ asset(img/lite/partneri/omcafe.png) }})"></div>
+                <div alt="omcafe" class="div-responsive_img" style="background-image: url({{ asset('/img/lite/partneri/omcafe.png') }})"></div>
             </a>
         </div>
         <div class="logoDiv-medij col-md-3 col-4">
             <a href="https://violeta.com" target="_blank">
-                <div alt="violeta" class="div-responsive_img" style="background-image: url({{ asset(img/lite/partneri/violeta.png) }})"></div>
+                <div alt="violeta" class="div-responsive_img" style="background-image: url({{ asset('/img/lite/partneri/violeta.png') }})"></div>
             </a>
         </div>
         <div class="logoDiv-medij col-md-3 col-4">
             <a href="https://www.podravka.com" target="_blank">
-                <div alt="podravka" class="div-responsive_img" style="background-image: url({{ asset(img/lite/partneri/podravka.png) }})"></div>
+                <div alt="podravka" class="div-responsive_img" style="background-image: url({{ asset('/img/lite/partneri/podravka.png') }})"></div>
             </a>
         </div>
     </div>
