bed53239f4ae795429c1b6315070ee245681a7bb
Tarik Horozovic <thorozovic1@etf.unsa.ba>
2020-12-20 22:14:22 +0100

    ispravljeni mediji

--- resources/views/lite/lite-partials/mediji.blade.php
+++ resources/views/lite/lite-partials/mediji.blade.php
@@ -6,29 +6,30 @@
         <div class="logoDiv-medij col-md-3 col-4">
             <a href="https://www.akta.ba" target="_blank">
                 <div alt="https://www.akta.ba" class="div-responsive_img"
-                    style="background-image: url({{ asset('/img/lite/mediji/akta1.png') }})"></div>
+                    style="background-image: url({{ asset('/img/lite/mediji/akta2.png') }})"></div>
             </a>
         </div>
-    </div>
-
-    <div class="logoDiv-medij col-md-3 col-4">
-        <a href="https://www.mreza-mira.net" target="_blank">
-            <div alt="https://www.mreza-mira.net" class="div-responsive_img"
-                style="background-image: url({{ asset('/img/lite/mediji/mrezaZaIzgradnjuMira.png') }})"></div>
-        </a>
-    </div>
+  
+        <div class="logoDiv-medij col-md-3 col-4">
+            <a href="https://www.mreza-mira.net" target="_blank">
+                <div alt="https://www.mreza-mira.net" class="div-responsive_img"
+                    style="background-image: url({{ asset('/img/lite/mediji/mrezaZaIzgradnjuMira.png') }})"></div>
+            </a>
+        </div>
     
-    <div class="logoDiv-medij col-md-3 col-4">
-        <a href="https://hocu.ba/" target="_blank">
-            <div alt="hocu.ba" class="div-responsive_img" width="40px"
-                style="background-image: url({{ asset('/img/lite/mediji/hocu.png') }})"></div>
-        </a>
-    </div>
-    <div class="logoDiv-medij col-md-3 col-4">
-        <a href="https://studomat.ba/" target="_blank">
-            <div alt="studomat" class="div-responsive_img"
-                style="background-image: url({{ asset('/img/lite/mediji/studomat.png') }})"></div>
-        </a>
+        <div class="logoDiv-medij col-md-3 col-4">
+            <a href="https://hocu.ba/" target="_blank">
+                <div alt="hocu.ba" class="div-responsive_img" width="40px"
+                    style="background-image: url({{ asset('/img/lite/mediji/hocu.png') }})"></div>
+            </a>
+        </div>
+
+        <div class="logoDiv-medij col-md-3 col-4">
+            <a href="https://studomat.ba/" target="_blank">
+                <div alt="studomat" class="div-responsive_img"
+                    style="background-image: url({{ asset('/img/lite/mediji/studomat.png') }})"></div>
+            </a>
+        </div>
     </div>
     <div class="row justify-content-center prvi_red pt-5">
         SSA LITE Summer 2020 - Medijski pokrovitelji
